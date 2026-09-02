1e0d523dd084245c0d4e8f66a306192c0908e3a1
user <user@192.168.34.120>
2026-09-02 12:46:56 +0200

    Fertige Implementierung der TaschenrechnerTest-Klasse

--- Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
@@ -19,20 +19,45 @@ public class TaschenrechnerTest {
 		System.out.println("| Options:                 |");
 		System.out.println("|        1. Addieren       |");
 		System.out.println("|        2. Subtrahieren   |");
-		System.out.println("|        3. Dividieren     |");
-		System.out.println("|        4. Exit           |");
+		System.out.println("|        3. Multiplizieren |");
+		System.out.println("|        4. Dividieren     |");
+		System.out.println("|        5. Exit           |");
 		System.out.println("============================");
 		System.out.print(" Select option: ");
 		swValue = myScanner.next().charAt(0);
-
+		int i = 0;
+		int e = 0;
 		// Switch construct
 		switch (swValue) {
 		case '1':
-			System.out.println("4 + 5 = " + ts.add(4, 5));
+			System.out.print("1. Summand: ");
+			i = myScanner.nextInt();
+			System.out.print("1. Summand: ");
+			e = myScanner.nextInt();
+			System.out.println(i+" + "+e+" = "+ts.add(i, e));
 			break;
-			
-			
-		  //  add your code here
+		case '2':
+			System.out.print("Minnuend: ");
+			i = myScanner.nextInt();
+			System.out.print("Subtrahend: ");
+			e = myScanner.nextInt();
+			System.out.println(i+" - "+e+" = "+ts.sub(i, e));
+			break;
+		case '3':
+			System.out.print("1. Faktor: ");
+			i = myScanner.nextInt();
+			System.out.print("2.. Summand: ");
+			e = myScanner.nextInt();
+			System.out.println(i+" * "+e+" = "+ts.mul(i, e));
+			break;
+		case '4':
+			System.out.print("Divident: ");
+			i = myScanner.nextInt();
+			System.out.print("Divisor: ");
+			e = myScanner.nextInt();
+			System.out.println(i+" / "+e+" = "+ts.div(i, e));
+			break;	
+		case '5': System.exit(0); 
 		  
 		  
 		default:

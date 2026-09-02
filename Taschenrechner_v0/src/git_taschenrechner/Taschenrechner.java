822b56d12d09f0641d923bd347ed9828f4048b4d
user <user@192.168.34.120>
2026-09-02 12:31:03 +0200

    Hinzufügen der Taschenrechner-Klasse

---
+++ Taschenrechner_v0/src/git_taschenrechner/Taschenrechner.java
@@ -0,0 +1,21 @@
+package git_taschenrechner;
+
+public class Taschenrechner {
+
+	public double add(double zahl1, double zahl2) {
+		return zahl1 + zahl2;
+	}
+	
+	public double sub(double zahl1, double zahl2){
+		// has to be implemented
+	}
+	
+	public double mul(double zahl1, double zahl2){
+		// has to be implemented
+	}
+
+	public double div(double zahl1, double zahl2){
+		// has to be implemented
+	}
+	
+}

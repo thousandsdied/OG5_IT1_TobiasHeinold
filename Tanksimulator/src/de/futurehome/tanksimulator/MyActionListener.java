6a054d441210f13e01e4d2f08d90ff06e27a5463
tobiasHeinold <user@192.168.34.115>
2026-09-09 12:25:00 +0200

    OOM/OOP 4. Fertiger Verbrauch-Button Funktionsweise des Verbrauch-Buttons zum ActionListener hinzugefügt.

--- Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
+++ Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
@@ -21,6 +21,14 @@ public class MyActionListener implements ActionListener {
 
 			 f.lblFuellstand.setText(""+fuellstand);
 		}
+		
+		if (obj == f.btnVerbrauchen) {
+			 double fuellstand = f.myTank.getFuellstand();
+			 fuellstand = fuellstand - 2;
+			 f.myTank.setFuellstand(fuellstand);
+
+			 f.lblFuellstand.setText(""+fuellstand);
+		}
 
 	}
 }
